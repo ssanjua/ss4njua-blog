e60c5c2c2ed8f8a2c4f6379e319414f8e7bfa7f5
ssanjua <ppaupallares@gmail.com>
2024-09-03 14:53:06 +0200

    add theme light/dark function

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,52 @@
 /** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+	return ({ opacityValue }) => {
+		if (opacityValue !== undefined) {
+			return `rgba(var(${variableName}), ${opacityValue})`;
+		}
+		return `rgb(var(${variableName}))`;
+	};
+}
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			textColor: {
+				skin: {
+					base: withOpacity("--color-text-base"),
+					accent: withOpacity("--color-text-accent"),
+				},
+			},
+			backgroundColor: {
+				skin: {
+					fill: withOpacity("--color-fill"),
+					card: withOpacity("--color-card"),
+					accent: withOpacity("--color-accent"),
+				},
+			},
+			fontFamily: {
+				mono: ["Martian Mono", "monospace"],
+				inter: ["Inter", "sans-serif"],
+			},
+			borderColor: {
+				skin: {
+					line: withOpacity("--color-border"),
+					accent: withOpacity("--color-border-accent"),
+				},
+			},
+			boxShadow: {
+				custom: 'var(--shadow-custom)',
+				accent: 'var(--shadow-custom-accent)',
+			},
+			fill: {
+				skin: {
+					base: withOpacity("--color-text-base"),
+					accent: withOpacity("--color-text-accent"),
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
